c68e1a2918db64ea025e1fb5ffb20da4a8c36fea
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-05 19:12:12 -0300

    create spring boot project

---
+++ src/main/java/com/github/robsonsf/designpatternchallenge/DesignPatternChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.github.robsonsf.designpatternchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/robsonsf/designpatternchallenge/DesignPatternChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.robsonsf.designpatternchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
