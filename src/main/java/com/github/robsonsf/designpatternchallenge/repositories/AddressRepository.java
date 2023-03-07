408fe385b5f9e72d6002e92c8684965f7e176cc4
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-07 01:07:24 -0300

    refactor repositories

--- src/main/java/com/github/robsonsf/designpatternchallenge/repositories/AddressRepository.java
+++ src/main/java/com/github/robsonsf/designpatternchallenge/repositories/AddressRepository.java
@@ -1,8 +1,10 @@
 package com.github.robsonsf.designpatternchallenge.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.github.robsonsf.designpatternchallenge.entities.Address;
 
-public interface AddressRepository extends CrudRepository<Address, String> {
+@Repository
+public interface AddressRepository extends JpaRepository<Address, String> {
 }
--- src/main/java/com/github/robsonsf/designpatternchallenge/repositories/ClientRepository.java
+++ src/main/java/com/github/robsonsf/designpatternchallenge/repositories/ClientRepository.java
@@ -1,9 +1,10 @@
 package com.github.robsonsf.designpatternchallenge.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.github.robsonsf.designpatternchallenge.entities.Client;
 
-public interface ClientRepository extends CrudRepository<Client, Long>{
-
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long>{
 }
