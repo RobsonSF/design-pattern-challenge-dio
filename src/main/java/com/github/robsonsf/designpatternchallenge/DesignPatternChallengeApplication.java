9d4a8742c9b2c530bf374d234f7c87d6179f6c44
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-07 01:04:26 -0300

    add Bean EnableFeignClients

--- src/main/java/com/github/robsonsf/designpatternchallenge/DesignPatternChallengeApplication.java
+++ src/main/java/com/github/robsonsf/designpatternchallenge/DesignPatternChallengeApplication.java
@@ -2,8 +2,10 @@ package com.github.robsonsf.designpatternchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DesignPatternChallengeApplication {
 
 	public static void main(String[] args) {
